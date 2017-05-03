6971ad41abca2faed034e49510d9be092fb8dde4
Kenshi Shiode <atemu1212@yahoo.co.jp>
2017-05-03 14:27:45 +0900

    quiz.htmlからquiz.phpに変更してphpが使えるように変更しました

---
+++ quiz.php
@@ -0,0 +1,26 @@
+<?php
+	if(isset($_GET["country"])){
+		$country=$_GET["country"];
+	}
+	switch($country){
+		case "America":
+			 break;
+		case "Japan":
+			break;
+		case "Canada":
+			break;
+		default:
+			break;
+	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MapTweetQuiz</title>
+</head>
+<body>
+	
+</body>
+</html>
