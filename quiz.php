80e0bc7114ce4ac2142242ec4e630c043e84217c
Kenshi Shiode <atemu1212@yahoo.co.jp>
2017-05-03 14:48:14 +0900

    とりあえずモックの有名人Twitterを連想配列にまとめてます

--- quiz.php
+++ quiz.php
@@ -1,11 +1,33 @@
 <?php
 	require_once("config.php");//TwitterAPIキー情報を読み込む
 
+	$people=array(
+		"Canada"=>array(
+			"Avril Lavigne"=>"AvrilLavigne",
+			"Justin Bieber"=>"justinbieber",
+			"Celine Dion"=>"celinedion",
+			"Jim Carrey"=>"JimCarrey"
+		),
+		"America"=>array(//後からアメリカ人に直す
+			"Avril Lavigne"=>"AvrilLavigne",
+			"Justin Bieber"=>"justinbieber",
+			"Celine Dion"=>"celinedion",
+			"Jim Carrey"=>"JimCarrey"
+		),
+		"Japan"=>array(//後から日本人に直す
+			"Avril Lavigne"=>"AvrilLavigne",
+			"Justin Bieber"=>"justinbieber",
+			"Celine Dion"=>"celinedion",
+			"Jim Carrey"=>"JimCarrey"
+		)
+	);
 
 	if(isset($_GET["country"])){
 		$country=$_GET["country"];
 	}
+
 	$tweets=array();
+	
 	switch($country){
 		case "America":
 			//アメリカの有名人4人の中からランダムに選択する
